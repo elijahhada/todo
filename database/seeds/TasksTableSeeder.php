970c5ea5f31038bf0c1e3a6075d3cace3fc96c91
Elijah <hadasevich.e@gmail.com>
2020-02-13 19:24:58 +0600

    change in TaskTableSeeder

--- database/seeds/TasksTableSeeder.php
+++ database/seeds/TasksTableSeeder.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Database\Seeder;
 use App\Task;
-use App\User;
 
 class TasksTableSeeder extends Seeder
 {
@@ -15,21 +14,18 @@ class TasksTableSeeder extends Seeder
     {
         Task::truncate();
 
-        $user1 = User::find(2);
-        $user2 = User::find(4);
-
         $task1 = Task::create([
             'title' => 'Выгулять собаку',
             'time' => \Carbon\Carbon::now()->addDays(2),
             'completed' => 0,
-            'user_id' => $user1->id
+            'user_id' => 2
         ]);
 
         $task2 = Task::create([
             'title' => 'Сделать покупки',
             'time' => \Carbon\Carbon::now()->subDays(2),
             'completed' => 1,
-            'user_id' => $user1->id
+            'user_id' => 4
         ]);
 
         $task3 = Task::create([
